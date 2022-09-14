cb1e7b213996027723a24c9e5dbad9e6ea04f52a
Set3mF <evdak@icloud.com>
2022-09-14 17:58:23 +0300

    +++

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from load_equip import LoadEquip
-import load_equip
 
 app = Flask(__name__)
 
@@ -9,12 +8,10 @@ def index_page():
     return render_template('index.html')
 
 
-@app.route('/choose-hero')
+@app.route('/choose-hero/')
 def start_game():
     result = LoadEquip()
-    result.load_weapons()
-    result.load_armors()
-    return render_template('hero_choosing.html', result=result.res_all)
+    return render_template('hero_choosing.html', result=result.load)
 
 if __name__ == "__main__":
     app.run(port=80, debug=True)
--- load_equip.py
+++ load_equip.py
@@ -4,24 +4,25 @@ import json
 class LoadEquip:
     res_all = {}
 
-    def load_json(self) -> str:
+    def _load_json(self) -> str:
         with open('data/equipment.json', 'r', encoding='utf-8') as f:
             return json.load(f)
 
-    def load_weapons(self) -> None:
-        pars = lambda: [x['name'] for x in self.load_json()['weapons']]
+    def _load_weapons(self) -> None:
+        pars = lambda: [x['name'] for x in self._load_json()['weapons']]
         result = self.res_all.update({'weapons': pars()})
 
-
-    def load_armors(self) -> None:
-        pars = lambda: [x['name'] for x in self.load_json()['armors']]
+    def _load_armors(self) -> None:
+        pars = lambda: [x['name'] for x in self._load_json()['armors']]
         result = self.res_all.update({'armors': pars()})
 
-    def test(self):
-        res = 
-        return res()
+    def _load_class(self):
+         self.res_all.update({"classes": ["Воин", "Вор","Орк","Эльф","Гном"]})
 
+    @property
+    def load(self):
+        self._load_class()
+        self._load_armors()
+        self._load_weapons()
+        return self.res_all
 
-x = LoadEquip()
-x.test()
-print(x.test())
